eef0c16aa324b8495be4f86d2c240d8d33d04c09
“Oatcharicha” <oatchari.c@ku.th>
2024-07-31 19:52:19 +0700

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
                 <a href="/chatgpt">chatGPT</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
-                <a href="/evaluation">การประเมินนโยบายสาธารณะ</a>
+                <a href="/productlist">การประเมินนโยบายสาธารณะ</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
                 <a href="/contact">ติดต่อ</a>
